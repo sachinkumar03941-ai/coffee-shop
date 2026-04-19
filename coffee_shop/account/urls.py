e58f3309bad298e26f56a0b7fbe7c6b733b7dc5e
sachinkumar03941-ai <sachinkumar03941@gmail.com>
2026-04-19 12:48:42 +0530

    Delete coffee_shop directory

--- coffee_shop/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from django.conf.urls.static import static
-from django.conf import settings
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('register/', views.CoffeeCornerRegisterView.as_view(), name='register')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
